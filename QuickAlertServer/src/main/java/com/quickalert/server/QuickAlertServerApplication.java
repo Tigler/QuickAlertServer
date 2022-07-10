d25afb5703127c86ad7d70d0d76ccdf6f981f2dc
tigler <tigler777@gmail.com>
2022-07-10 15:53:42 +0700

    init project

---
+++ QuickAlertServer/src/main/java/com/quickalert/server/QuickAlertServerApplication.java
@@ -0,0 +1,13 @@
+package com.quickalert.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickAlertServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickAlertServerApplication.class, args);
+    }
+
+}
---
+++ QuickAlertServer/src/test/java/com/quickalert/server/QuickAlertServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickalert.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickAlertServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
